88321b5480cae734ede94f6322fa31ac87fd5e50
samikshasm <mailarpwarss@slu.edu>
2018-04-11 18:57:48 -0500

    Make Morning Report pop up before morning questionnaire

--- app/src/main/java/com/samalex/slucapstone/MorningQS.java
+++ app/src/main/java/com/samalex/slucapstone/MorningQS.java
@@ -142,7 +142,7 @@ public class MorningQS extends AppCompatActivity {
         //creates all of the onClick listeners for all of the buttons for questions
         View.OnClickListener handler1 = new View.OnClickListener() {
             public void onClick(View view) {
-                if(group.equals("experimental")){
+              /*  if(group.equals("experimental")){
                     Log.e("morningQS", "experimental");
                     Intent switchToMorningReport = new Intent(MorningQS.this, MorningReport.class);
                     startActivity(switchToMorningReport);
@@ -163,7 +163,7 @@ public class MorningQS extends AppCompatActivity {
                     goToStart.putExtra("Start Activity", startActivity);
                     startActivity(goToStart);
                     finish();
-                }
+                }*/
 
                 writeOralToDB(oral);
                 //writeOralConsentToDB(oralConsentStr);
@@ -184,6 +184,14 @@ public class MorningQS extends AppCompatActivity {
                 costButton = (RadioButton) findViewById(selectedId);
                 drinkCost = costButton.getText().toString();
                 writeCostTotaltoDB(drinkCost);
+                finish();
+
+                startActivity1 = "start";
+                storeScreen(startActivity1);
+                storeNumDrinks(0);
+                Intent goToStart = new Intent(MorningQS.this, StartActivity.class);
+                goToStart.putExtra("Start Activity", startActivity);
+                startActivity(goToStart);
                 finish();
             }
         };
