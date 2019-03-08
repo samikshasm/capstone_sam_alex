a12af591a90ad4dde04d8d35de473c2cdf488b76
Pannapat Chanpaisaeng <ch.pannapat@gmail.com>
2019-03-08 13:06:39 -0600

    Add a comment

--- app/src/main/java/com/samalex/slucapstone/MorningQS.java
+++ app/src/main/java/com/samalex/slucapstone/MorningQS.java
@@ -373,6 +373,7 @@ public class MorningQS extends AppCompatActivity {
                 writeStressValueToDB(stress_value);
                 finish();
 
+                // The "none" and "control" group will not see the Morning Summary Report
                 if (group.equals("control") | group.equals("none")) {
                     startActivity1 = "start";
                     storeScreen(startActivity1);
