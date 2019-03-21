3a71a8e6737ad032a125f36192bbc50f82506263
Pannapat Chanpaisaeng <ch.pannapat@gmail.com>
2019-03-21 14:43:42 -0500

    Add all contants to BoozymeterApplication class

--- app/src/main/java/com/samalex/slucapstone/BoozymeterApplication.java
+++ app/src/main/java/com/samalex/slucapstone/BoozymeterApplication.java
@@ -4,20 +4,11 @@ import android.app.AlarmManager;
 import android.app.Application;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.icu.util.TimeUnit;
 
 import java.util.Calendar;
 
 public class BoozymeterApplication extends Application {
-    private boolean isDebug = false;
-
-    public boolean isDebug() {
-        return isDebug;
-    }
-
-    public void setDebug(boolean debug) {
-        isDebug = debug;
-    }
-
     @Override
     public void onCreate() {
         super.onCreate();
@@ -51,4 +42,31 @@ public class BoozymeterApplication extends Application {
         }
     }
 
+    // Official settings
+    final public static int CYCLE_OFFSET = 5 * 60 * 60 * 1000; // 5 hours (from start of natural cycle)
+    final public static int CYCLE_LENGTH = 24 * 60 * 60 * 1000; // 1 day
+    final public static int NUM_CYCLES = 21; // 21 cycles
+    final public static int SURVEY_OFFSET = 8 * 60 * 60 * 1000; // 8 hours (from start of natural cycle)
+    final public static int EVENING_REMINDER_OFFSET = 18 * 60 * 60 * 1000; // 18 hours (from start of natural cycle)
+    final public static int IN_EPISODE_REMINDER_INTERVAL = 30 * 60 * 1000; // 30 minutes
+    final public static int NUM_IN_EPISODE_REMINDER = 4; // 4 reminders every IN_EPISODE_REMINDER_INTERVAL
+
+//    // Testing settings
+//    final public static int CYCLE_OFFSET = 5 * 60 * 1000;  // 5 minutes (from start of natural cycle)
+//    final public static int CYCLE_LENGTH = 24 * 60 * 1000; // 24 minutes
+//    final public static int NUM_CYCLES = 3; // 3 cycles
+//    final public static int SURVEY_OFFSET = 8 * 60 * 1000; // 8 minutes (from start of natural cycle)
+//    final public static int EVENING_REMINDER_OFFSET = 18 * 60 * 1000; // 18 minutes (from start of natural cycle)
+//    final public static int IN_EPISODE_REMINDER_INTERVAL = 30 * 1000; // 30 seconds
+//    final public static int NUM_IN_EPISODE_REMINDER = 4; // 4 reminders every IN_EPISODE_REMINDER_INTERVAL
+
+    // Global settings
+    private boolean isDebug = false;
+
+    public boolean isDebug() {
+        return isDebug;
+    }
+    public void setDebug(boolean debug) {
+        isDebug = debug;
+    }
 }
