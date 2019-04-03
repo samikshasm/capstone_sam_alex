80d014dd31d121f9f6a2590b0dd2c1a0c14e7ed8
Pannapat Chanpaisaeng <ch.pannapat@gmail.com>
2019-04-03 14:06:18 -0500

    Add LiveReportData class to store data that will be displayed as a live report

--- app/src/main/java/com/samalex/slucapstone/LiveReportData.java
+++ app/src/main/java/com/samalex/slucapstone/LiveReportData.java
@@ -1,4 +1,56 @@
 package com.samalex.slucapstone;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class LiveReportData {
+    private int numDrinks = 0;
+    private int ouncesConsumed = 0;
+    private int caloriesConsumed = 0;
+    private double averageCost = 0;
+    private Map<String, Float> drinkTypePercentage = new LinkedHashMap<>();
+
+    public Map<String, Float> getDrinkTypePercentage() {
+        return drinkTypePercentage;
+    }
+
+    public void setDrinkTypePercentage(Map<String, Float> drinkTypePercentage) {
+        this.drinkTypePercentage = drinkTypePercentage;
+    }
+
+    public void setNumDrinks(int numDrinks) {
+        this.numDrinks = numDrinks;
+    }
+
+    public void setOuncesConsumed(int ouncesConsumed) {
+        this.ouncesConsumed = ouncesConsumed;
+    }
+
+    public void setCaloriesConsumed(int caloriesConsumed) {
+        this.caloriesConsumed = caloriesConsumed;
+    }
+
+    public void setAverageCost(double averageCost) {
+        this.averageCost = averageCost;
+    }
+
+    public int getNumDrinks() {
+        return numDrinks;
+    }
+
+    public int getOuncesConsumed() {
+        return ouncesConsumed;
+    }
+
+    public int getCaloriesConsumed() {
+        return caloriesConsumed;
+    }
+
+    public double getAverageCost() {
+        return averageCost;
+    }
+
+    public double getLitersConsumed(){
+        return ouncesConsumed * 0.03;
+    }
 }
