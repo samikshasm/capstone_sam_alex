7fc0727dcd35e9634b075bb4e1c86a0f71d92312
Pannapat Chanpaisaeng <ch.pannapat@gmail.com>
2019-04-03 14:08:34 -0500

    Add getDrinkPercentages and getAverageCost functions to CalculationUtil class

--- app/src/main/java/com/samalex/slucapstone/CalculationUtil.java
+++ app/src/main/java/com/samalex/slucapstone/CalculationUtil.java
@@ -1,6 +1,7 @@
 package com.samalex.slucapstone;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CalculationUtil {
@@ -83,5 +84,61 @@ public class CalculationUtil {
         percentDrinks.put("liquor", percentLiquor);
         percentDrinks.put("wine", percentWine);
         return percentDrinks;
+    }
+
+    public static Map<String, Float> getDrinkPercentages(List<String> drinkTypeList) {
+        int numDrinks = drinkTypeList.size();
+        Integer numWine = 0;
+        Integer numLiquor = 0;
+        Integer numBeer = 0;
+
+        for (int i = 0; i < drinkTypeList.size(); i++) {
+            String drinkType = drinkTypeList.get(i);
+            switch (drinkType) {
+                case "wine":
+                    numWine++;
+                    break;
+                case "liquor":
+                    numLiquor++;
+                    break;
+                case "beer":
+                    numBeer++;
+                    break;
+            }
+        }
+
+        float percentBeer = ((float) numBeer / (float) numDrinks) * 100;
+        float percentLiquor = ((float) numLiquor / (float) numDrinks) * 100;
+        float percentWine = ((float) numWine / (float) numDrinks) * 100;
+
+        Map<String, Float> percentDrinks = new LinkedHashMap<>();
+        percentDrinks.put("beer", percentBeer);
+        percentDrinks.put("liquor", percentLiquor);
+        percentDrinks.put("wine", percentWine);
+        return percentDrinks;
+    }
+
+    public static double getAverageCost(List<String> costList) {
+        double avgCost = 0;
+        for (int i = 0; i < costList.size(); i++) {
+            switch (costList.get(i)) {
+                case COST_OPTION_1:
+                    avgCost += AVG_COST_OPTION_1;
+                    break;
+                case COST_OPTION_2:
+                    avgCost += AVG_COST_OPTION_2;
+                    break;
+                case COST_OPTION_3:
+                    avgCost += AVG_COST_OPTION_3;
+                    break;
+                case COST_OPTION_4:
+                    avgCost += AVG_COST_OPTION_4;
+                    break;
+                case COST_OPTION_5:
+                    avgCost += AVG_COST_OPTION_5;
+                    break;
+            }
+        }
+        return avgCost;
     }
 }
