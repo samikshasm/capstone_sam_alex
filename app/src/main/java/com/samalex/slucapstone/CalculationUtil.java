c39e9b60aaf689688e15c6eec24c4d73c0b3793b
Pannapat Chanpaisaeng <ch.pannapat@gmail.com>
2019-04-04 00:08:04 -0500

    Add getLiveData function for squash all episode data into one LiveReportData object

--- app/src/main/java/com/samalex/slucapstone/CalculationUtil.java
+++ app/src/main/java/com/samalex/slucapstone/CalculationUtil.java
@@ -1,5 +1,7 @@
 package com.samalex.slucapstone;
 
+import com.samalex.slucapstone.dto.DrinkAnswer;
+
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,6 +19,9 @@ public class CalculationUtil {
     private static final double AVG_COST_OPTION_4 = 13.00;
     private static final double AVG_COST_OPTION_5 = 16.00;
 
+    public static final int BEER_SERVING_SIZE = 12;
+    public static final int WINE_SERVING_SIZE = 4;
+    public static final int LIQUOR_SERVING_SIZE = 1;
     public static final Map<String, Integer> DRINK_SERVING_SIZE_MAP = new LinkedHashMap<>();
     public static final Integer CALORIES_PER_SERVING = 100;
 
@@ -54,7 +59,7 @@ public class CalculationUtil {
         return avgCost;
     }
 
-    public static Map<String, Float> getDrinkPercentages(Map<String, String> drinkseMap) {
+    public static Map<String, Float> oldGetDrinkPercentages(Map<String, String> drinkseMap) {
         int numDrinks = drinkseMap.size();
         Integer numWine = 0;
         Integer numLiquor = 0;
@@ -86,7 +91,7 @@ public class CalculationUtil {
         return percentDrinks;
     }
 
-    public static Map<String, Float> getDrinkPercentages(List<String> drinkTypeList) {
+    public static Map<String, Float> oldGetDrinkPercentages(List<String> drinkTypeList) {
         int numDrinks = drinkTypeList.size();
         Integer numWine = 0;
         Integer numLiquor = 0;
@@ -140,5 +145,112 @@ public class CalculationUtil {
             }
         }
         return avgCost;
+    }
+
+    public static Map<String, Float> getDrinkPercentages(List<DrinkAnswer> allDrinkAnswers) {
+        int numDrinks = allDrinkAnswers.size();
+
+        Integer numWine = 0;
+        Integer numLiquor = 0;
+        Integer numBeer = 0;
+
+        for (int i = 0; i < allDrinkAnswers.size(); i++) {
+            DrinkAnswer answer = allDrinkAnswers.get(i);
+
+            // TODO: change type from string to enum
+            switch (answer.getType()) {
+                case "wine":
+                    numWine++;
+                    break;
+                case "liquor":
+                    numLiquor++;
+                    break;
+                case "beer":
+                    numBeer++;
+                    break;
+            }
+
+
+        }
+
+        float percentBeer = ((float) numBeer / (float) numDrinks) * 100;
+        float percentLiquor = ((float) numLiquor / (float) numDrinks) * 100;
+        float percentWine = ((float) numWine / (float) numDrinks) * 100;
+
+        Map<String, Float> percentDrinks = new LinkedHashMap<>();
+        percentDrinks.put("beer", percentBeer);
+        percentDrinks.put("liquor", percentLiquor);
+        percentDrinks.put("wine", percentWine);
+        return percentDrinks;
+    }
+
+    public static LiveReportData getLiveData(List<DrinkAnswer> allDrinkAnswers) {
+
+        int numDrinks = allDrinkAnswers.size();
+
+        int totalNumWine = 0, totalNumLiquor = 0, totalNumBeer = 0;
+        int totalOuncesConsumed = 0;
+        long totalCaloriesConsumed = 0;
+        int totalAverageCost = 0;
+
+        for (int i = 0; i < allDrinkAnswers.size(); i++) {
+            DrinkAnswer answer = allDrinkAnswers.get(i);
+
+            int drinkSize = answer.getSize();
+            totalOuncesConsumed += drinkSize;
+
+            // for drink type percentage
+            // TODO: change type from string to enum
+            switch (answer.getType()) {
+                case "wine":
+                    totalNumWine++;
+                    totalCaloriesConsumed += (CalculationUtil.CALORIES_PER_SERVING * (drinkSize / WINE_SERVING_SIZE));
+                    break;
+
+                case "liquor":
+                    totalNumLiquor++;
+                    totalCaloriesConsumed += (CalculationUtil.CALORIES_PER_SERVING * (drinkSize / LIQUOR_SERVING_SIZE));
+                    break;
+
+                case "beer":
+                    totalNumBeer++;
+                    totalCaloriesConsumed += (CalculationUtil.CALORIES_PER_SERVING * (drinkSize / BEER_SERVING_SIZE));
+                    break;
+            }
+
+            switch (answer.getCost()) {
+                case COST_OPTION_1:
+                    totalAverageCost += AVG_COST_OPTION_1;
+                    break;
+                case COST_OPTION_2:
+                    totalAverageCost += AVG_COST_OPTION_2;
+                    break;
+                case COST_OPTION_3:
+                    totalAverageCost += AVG_COST_OPTION_3;
+                    break;
+                case COST_OPTION_4:
+                    totalAverageCost += AVG_COST_OPTION_4;
+                    break;
+                case COST_OPTION_5:
+                    totalAverageCost += AVG_COST_OPTION_5;
+                    break;
+            }
+        }
+
+        // calculate percentage of each drink type
+        float beerPercentage = ((float) totalNumBeer / (float) numDrinks) * 100;
+        float liquorPercentage = ((float) totalNumLiquor / (float) numDrinks) * 100;
+        float winePercentage = ((float) totalNumWine / (float) numDrinks) * 100;
+
+        LiveReportData liveReportData = new LiveReportData(
+                numDrinks,
+                totalOuncesConsumed,
+                totalCaloriesConsumed,
+                totalAverageCost,
+                beerPercentage,
+                winePercentage,
+                liquorPercentage);
+
+        return liveReportData;
     }
 }
