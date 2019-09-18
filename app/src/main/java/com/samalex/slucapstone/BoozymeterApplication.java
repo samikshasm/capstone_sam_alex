488d418fc69525812cbcfbb99c87f920deec1e37
Pannapat Chanpaisaeng <ch.pannapat@gmail.com>
2019-09-18 11:50:57 -0500

    Configure for 6-day testing on Wed 4 September, 2019

--- app/src/main/java/com/samalex/slucapstone/BoozymeterApplication.java
+++ app/src/main/java/com/samalex/slucapstone/BoozymeterApplication.java
@@ -21,12 +21,12 @@ public class BoozymeterApplication extends Application {
 //    public static final long IN_EPISODE_REMINDER_INTERVAL = 30 * 60 * 1000; // 30 minutes
 
     // Testing settings
-//    public static final long CYCLE_OFFSET = 0 * 60 * 1000;    // 0 mins
-//    public static final long CYCLE_LENGTH = 3 * 60 * 1000;    //  3 mins
-//    public static final int NUM_CYCLES = 3;
-//    public static final long SURVEY_OFFSET = 4 * 60 * 1000;  //  4 mins after cycle start
-//    public static final long EVENING_REMINDER_OFFSET = 2 * 60 * 1000;    // 2 mins from cycle time which is 3 hours from cycle offset start
-//    public static final long IN_EPISODE_REMINDER_INTERVAL = 10 * 1000;   // 10 secconds
+    public static final long CYCLE_OFFSET = 0 * 60 * 1000;    // 0 mins
+    public static final long CYCLE_LENGTH = 3 * 60 * 1000;    //  3 mins
+    public static final int NUM_CYCLES = 6;
+    public static final long SURVEY_OFFSET = 4 * 60 * 1000;  //  4 mins after cycle start
+    public static final long EVENING_REMINDER_OFFSET = 2 * 60 * 1000;    // 2 mins from cycle time which is 3 hours from cycle offset start
+    public static final long IN_EPISODE_REMINDER_INTERVAL = 15 * 1000;   // 15 secconds
 
     //  Research team's test settings (04/05/2019)
 //    public static final long CYCLE_OFFSET = 5 * 60 * 60 * 1000; // 5 hours (from start of natural cycle)
@@ -37,12 +37,20 @@ public class BoozymeterApplication extends Application {
 //    public static final long IN_EPISODE_REMINDER_INTERVAL = 30 * 60 * 1000; // 30 minutes
 
     // Weekend test settings (08/20/2019)
-    public static final long CYCLE_OFFSET = 4 * 60 * 60 * 1000;     //  4-hour offset to force 12pm (noon) start of cycle #1
-    public static final long CYCLE_LENGTH = 8 * 60 * 60 * 1000;    //  8-hour cycles
-    public static final int NUM_CYCLES = 9;
-    public static final long SURVEY_OFFSET = 510 * 60 * 1000;  //  8 hours and 30 minutes beyond offset start
-    public static final long EVENING_REMINDER_OFFSET = 3 * 60 * 60 * 1000;    // 3 hours after offset start
-    public static final long IN_EPISODE_REMINDER_INTERVAL = 30 * 60 * 1000;   // 30 minutes
+//    public static final long CYCLE_OFFSET = 4 * 60 * 60 * 1000;     //  4-hour offset to force 12pm (noon) start of cycle #1
+//    public static final long CYCLE_LENGTH = 8 * 60 * 60 * 1000;    //  8-hour cycles
+//    public static final int NUM_CYCLES = 9;
+//    public static final long SURVEY_OFFSET = 510 * 60 * 1000;  //  8 hours and 30 minutes beyond offset start
+//    public static final long EVENING_REMINDER_OFFSET = 3 * 60 * 60 * 1000;    // 3 hours after offset start
+//    public static final long IN_EPISODE_REMINDER_INTERVAL = 30 * 60 * 1000;   // 30 minutes
+
+        // Pre-Official settings
+//    public static final long CYCLE_OFFSET = 5 * 60 * 60 * 1000; // 5 hours (from start of natural cycle)
+//    public static final long CYCLE_LENGTH = 24 * 60 * 60 * 1000; // 1 day
+//    public static final int NUM_CYCLES = 6; // 6 cycles
+//    public static final long SURVEY_OFFSET = 27 * 60 * 60 * 1000; // 8 am (27 hours from user start time that includes CYCLE_OFFSET)
+//    public static final long EVENING_REMINDER_OFFSET = 13 * 60 * 60 * 1000; // 6pm (13 hours from user start time that includes CYCLE_OFFSET)
+//    public static final long IN_EPISODE_REMINDER_INTERVAL = 30 * 60 * 1000; // 30 minutes
 
     // Global settings
     private boolean isDebug = false;
@@ -57,5 +65,9 @@ public class BoozymeterApplication extends Application {
 
     public static long getNextMorningSurveyTimeInMillis(long userStartTime, int currentCycle) {
         return userStartTime + (currentCycle * BoozymeterApplication.CYCLE_LENGTH) + BoozymeterApplication.SURVEY_OFFSET;
+    }
+
+    public static long getNextEveningRemonderTimeInMillis(long userStartTime, int currentCycle) {
+        return userStartTime + (currentCycle * BoozymeterApplication.CYCLE_LENGTH) + BoozymeterApplication.EVENING_REMINDER_OFFSET;
     }
 }
